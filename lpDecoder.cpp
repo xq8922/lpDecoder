c7d5cf426ba82493af8c54a9ae91c845a1a30647
xq8922 <xq8922@gmail.com>
2014-12-19 23:34:03 +0800

    修改:         lpDecoder.cpp

--- lpDecoder.cpp
+++ lpDecoder.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <time.h>
-#include <math.h>
-#include <stdlib.h>
+#include <sstream>
+#include <ctime>
+#include <cmath>
+//#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
 #include <glpk.h>
 using namespace std;
 
@@ -33,7 +36,7 @@ void read_file(){
 	fscanf(fp,"%d",&M);
 	fscanf(fp,"%d",&MaxColDegree);
 	fscanf(fp,"%d",&MaxRowDegree);
-	H = (int **)malloc(M*sizeof(int));
+	H = (int **)malloc(M*sizeof(int*));
 	for(i=0;i<M;i++){
 		H[i] = (int *)malloc(N*sizeof(int));
 	}
@@ -54,27 +57,30 @@ void read_file(){
 	A_V = (int **)malloc(N*sizeof(int *));
 	for(i = 0;i < N;i ++)
 		A_V[i] = (int *)malloc(MaxColDegree*sizeof(int));
-	for(i = 0;i < N;i++)
-		for(j = 0;j < MaxColDegree;j ++)
+	for(i = 0;i < N;i++){
+		for(j = 0;j < MaxColDegree;j ++){
 			A_V[i][j] = 0;
+		}
+	}
+	
 	for(i = 0;i < N;i ++){
 		for(j = 0;j < MaxColDegree;j ++){
 			fscanf(fp,"%d",&tmp);
 			A_V[i][j] = tmp-1;
-//			if(tmp > 0)
-//				H[tmp-1][i] = 1;
+			if(tmp > 0)
+				H[tmp-1][i] = 1;
 		}
 	}
 	B_C = (int **)malloc(M*sizeof(int*));
 	for(i = 0;i < M;i ++)
 		B_C[i] = (int *)malloc(MaxRowDegree*sizeof(int));
-	for(i = 0;i < M;i ++)
+	for(i = 0;i < M;i ++){
 		for(j = 0;j < MaxRowDegree;j++){
 			fscanf(fp,"%d",&tmp);
 			B_C[i][j] = tmp-1;
-			if(tmp > 0)
-				H[i][tmp-1] = 1;
+		//	H[i][tmp-1] = 1;
 		}
+	}
 	fclose(fp);
 	print_H();
 }
@@ -121,7 +127,7 @@ double Gauss(){
 	return ret;
 }
 
-void functionObjective(const double var){
+void functionObjective(const double &stdev){
 	int i;
 	initCode = (int *)malloc(N*sizeof(int));
 	recvCode = (double *)malloc(N*sizeof(double));
@@ -130,8 +136,8 @@ void functionObjective(const double var){
 	for(i = 0;i < N;i ++)
 		recvCode[i] = pow(-1,initCode[i]);//Because all-0
 	for(i = 0;i < N;i ++){
-		recvCode[i] += Gauss()*var;
-		recvCode[i] = 2*recvCode[i]/(var*var);
+		recvCode[i] += Gauss()*stdev;
+		recvCode[i] = 2*recvCode[i]/(stdev*stdev);
 	}
     FILE *fp;
     fp = fopen("objCoef","w");
@@ -145,7 +151,7 @@ void functionObjective(const double var){
 
     fclose(fp);
 	free(initCode);
-    free(recvCode);
+//	free(recvCode);
 }
 
 long factorial(int n){
@@ -157,17 +163,71 @@ long factorial(int n){
 	return result;
 }
 
-long combination(const int n,const int k){
-	if(n<k){
+long combination(const int n,int i){
+	if(n<i){
 		printf("error when call combination method");
 		return 0;
 	}
-	if(k==0 || k==n)
+	if(i==0 || i==n)
 		return 1;
-	else if(k == 1)
+	else if(i == 1)
 		return n;
 	else
-		return factorial(n)/(factorial(k)*factorial(n-k));
+	{
+		if (i > (n/2))   // C(n,i) = C(n,(n-i))
+		{
+			i = n-i;
+		}
+
+		//最简单的算法，返回 n!/i!*(n-i)! ，但非常容易溢出
+		//return factorial(n)/(factorial(i)*factorial(n-i));
+
+		//第二种算法，返回 n*(n-1)*(n-2)*...*(n-i+1) / i! ，并逐级约分
+		int *numerator; //存储分子的数组
+		int *denominator; //存储分母的数组
+		numerator = (int *)malloc(i*sizeof(int));
+		denominator = (int *)malloc((i-1)*sizeof(int));
+
+		int k,s;
+		for (k=0; k<i; k++)
+		{
+			numerator[k] = n-k; 
+		}//numerator[]存储n，n-1...n-i+1
+		for (k=0; k<i-1; k++)
+		{
+			denominator[k] = i-k;
+		}//denominator[]存储i，i-1...2 ,s[0]=i...s[i-2]=2
+		for (k=0; k<i; k++)
+		{
+			for (s=(i-1)-1; s>=0; s--)
+			{
+				if (denominator[s]==1)
+				{
+					continue;
+				}
+				if (numerator[k]%denominator[s] == 0)
+				{
+					numerator[k] /= denominator[s];
+					denominator[s] = 1;
+				}
+			}
+		}//从分子的最低位，和分母逐个进行约分，若已约分，则相应分子除以分母，分母为1
+		
+		long num=1, den=1;
+		for (k=0; k<i; k++)
+		{
+			num *= numerator[k];
+		}
+		for (k=0; k<i-1; k++)
+		{
+			den *= denominator[k];
+		}
+		
+		free(numerator);
+		free(denominator);
+
+		return num/den;
+	}
 }
 
 int combOddNum(const int n){
@@ -184,10 +244,10 @@ void matrix(){
 	int *B;
 	int i,j,k,offset=0,combNum,assign;
 	int max_sign_i;
-printf("MaxRowDe=%d\n",MaxRowDegree);
-printf("MaxColDe=%d\n",MaxColDegree);
+	printf("MaxRowDe=%d\n",MaxRowDegree);
+	printf("MaxColDe=%d\n",MaxColDegree);
  	wCount = combOddNum(MaxRowDegree);
-printf("wCount=%d\n",wCount);
+	printf("wCount=%d\n",wCount);
 	A = (int **)malloc((wCount)*sizeof(int*));
 	for(i = 0;i < wCount;i ++)
 		A[i] = (int *)malloc(MaxRowDegree*sizeof(int));
@@ -196,51 +256,15 @@ printf("wCount=%d\n",wCount);
 			A[i][j] = -1;
 	}
 	B = (int*) malloc (wCount*sizeof(int));
-	temp_B = (int*)malloc(wCount*sizeof(int));
+	temp_B = (int*)malloc(M*wCount*sizeof(int));
 	k = 1;
-/*
-	while(k <= MaxRowDegree){
-		int *c = (int *)malloc(k*sizeof(int));
-		for(i = 0;i < k;i++){
-			c[i] = i;
-		}
-		comNum = combination(MaxRowDegree,k);
-		for(int r = 0;r < comNum;r++)
-			B[r+offset] = k-1;
-		for(assign = 0;assign <= comNum-1;assign++){
-			for(i = 0;i < k;i++){
-				A[assign+offset][c[i]] = 1;
-			}
-			maxSignI = -1;			
-			for(i = 0;i < k;i ++){
-				if(c[i] < MaxRowDegree-k+i)
-					maxSignI = i;
-			}
-			if(maxSignI == -1){
-				if(assign != comNum-1){
-					printf("error when maxSignI");
-					return ;
-				}
-			}
-			if(maxSignI != -1){
-				c[maxSignI] += 1;
-				for(j = maxSignI + 1;j < k;j ++)
-					c[j] = c[j-1]+1;
-			}
-		}
-		offset += comNum;
-		k += 2;
-		free(c);
-	}*/
-
-  while( k <= MaxRowDegree )
-  {
+  	while( k <= MaxRowDegree )
+  	{
         int *c = (int *)malloc( k*sizeof(int) );
         for (i=0; i<k; i++)
         {
             c[i] = i;
         }
-
 
         combNum = combination( MaxRowDegree , k );
 
@@ -282,31 +306,8 @@ printf("wCount=%d\n",wCount);
         offset += combNum;
         k += 2;
         free(c);
-  }
-
-	printf("print A\n");
-	for(i = 0;i < 10;i ++){
-		for(j = 0;j < MaxRowDegree;j ++){
-			printf("%d ",A[i][j]);
-		}
-		printf("\n");
 	}
-/*	printf("print B\n");
-cout<<"[";
-    for ( i = 0; i < M ; i++)
-    {
-        for ( j = 0; j < wCount ; j++)
-        {
-            cout<<B[j]<<" ";
-        }
-    }
-    cout<<"]\n";
-*/
-//	for(j = 0;j < wCount; j++)
-//		printf("%d ",B[j]);
-	printf("\n");
-	rCount = M*wCount;
-	coef = (int **)malloc(rCount*sizeof(int*));
+	coef = (int **)malloc(rCount*N*sizeof(int*));
 	for(i = 0;i < rCount;i ++){
 		coef[i] = (int*)malloc(N*sizeof(int));
 		for(k = 0;k < N;k++)
@@ -343,9 +344,14 @@ cout<<"[";
 			temp_B[tmp++] = B[j];
        	}
    	}
-
     fprintf(fp,"]\n");
     fclose(fp);
+	
+	
+//	for(i = 0;i < rCount;i ++){
+//		printf("%d,",temp_B[i]);
+//		//std::cout << "value: " << temp_B[i] << std::endl;
+//	}
 /*
 	for ( i = 0; i < rCount; i++)
     {
@@ -356,86 +362,91 @@ cout<<"[";
 }
 
 int main(){
-	double var;
+	double stdev;
 	read_file();
 	srand((unsigned)time(NULL));
 	matrix();
-	printf("%d,%d\n",M,N);
+//	printf("%d,%d\n",M,N);
 	FILE *fp;
 	fp = fopen("result","w");
 	if(! fp){
 		printf("err when open file result");
 		exit(0);
 	}
-	const float ESP = 0.000001;
-	for(double snr = 2.0;snr <= 5.0;snr += 0.1){
+	for(double snr = 0.0;snr <= 0.0;snr += 0.1){
 		int errRate = 0;
 		long sumCount = 0;
 		int fifty = 50;
-int BEC = 0,WEC = 0;
-		var = sqrt(1.0/((2.0*(N-M)/N)*pow(10,snr/10)));
+		int BEC = 0;
+		int WEC = 0;
+		stdev=sqrt(1.0/((2.0*(N - M)/N)*pow(10,snr/10)));
+		int *ia = (int*)malloc((1+N*rCount)*sizeof(int));
+		int *ja = (int*)malloc((N*rCount+1)*sizeof(int));
+		double *ar = (double*)malloc((1+N*rCount)*sizeof(double));
+		double *x = (double *)malloc(sizeof(double)*(N+1));
 		while(true){
-			
-			functionObjective(var);
-			//decoding . judge if right sumCount++;else errRate++;
-			//if(resultCode == 0)sumCOunt++;else errRate++;
+			sumCount++;
+			functionObjective(stdev);
 			glp_prob *lp;
-			int *ia = (int*)malloc(rCount*sizeof(int));
-			int *ja = (int*)malloc(N*sizeof(int));
-			double *ar = (double*)malloc(N*rCount*sizeof(double));
-			int i,j,k=1;
-			double *x = (double *)malloc(sizeof(double)*N);
+			int i,j,k=0;			
 			lp = glp_create_prob();
-		glp_set_prob_name(lp,"lpdecoder");
-		glp_set_obj_dir(lp,GLP_MIN);
+			glp_set_prob_name(lp,"lpdecoder");
+			glp_set_obj_dir(lp,GLP_MIN);
 			glp_add_rows(lp,rCount);
-			for(i = 1;i <= rCount;i += 2){
-		glp_set_row_name(lp,i,"");//""+i
-		glp_set_row_bnds(lp,i+1,GLP_UP,0.0,temp_B[i-1]);
+			for(i = 0;i < rCount;i ++){
+				std::stringstream strOs;
+				strOs << i+1;
+			//	glp_set_row_name(lp,i+1,strOs.str().c_str());//""+i
+				glp_set_row_bnds(lp,i+1,GLP_UP,0.0,temp_B[i]);
 			}
-		glp_add_cols(lp,3);
-			for(i = 1;i < N;i += 3){
-		glp_set_col_name(lp,i,"");//""+x[i]
-		glp_set_col_bnds(lp,i,GLP_LO,0.0,0.0);
-		glp_set_obj_coef(lp,i,recvCode[i-1]);
+			glp_add_cols(lp,N);
+			for(i = 1;i <= N;i ++){
+//				std::stringstream strOs;
+//				strOs << i;
+//				string s;
+//				sprintf(s,"%d",i);
+	//			glp_set_col_name(lp,i,"x");//""+x[i]   
+//				glp_set_col_bnds(lp,i,GLP_LO,0.0,0.0);
+				glp_set_obj_coef(lp,i,recvCode[i-1]);
 			}
-		int tmp=0;
-		for(i = 0;i < rCount;i ++){
-			for(j = 0;j < N;j ++){
-			ia[i] = i,ja[j] = j,ar[tmp++] = coef[i][j];
+			int tmp=1;
+			for(i = 1;i <= rCount;i ++){
+				for(j = 1;j <= N;j ++){
+					ia[tmp] = i,ja[tmp] = j,ar[tmp++] = coef[i-1][j-1];
+//				std::cout << coef[i-1][j-1] << " ";	
+//				std::cout << endl;
+				}
 			}
-		}
-		glp_load_matrix(lp,tmp-1,ia,ja,ar);
-		glp_simplex(lp,NULL);
-		for(i = 0;i < N;i ++){
-		x[i] = glp_get_col_prim(lp,i+1);
-		printf("\nx = %g",x[i]);
-		}
-		glp_delete_prob(lp);
+			i--;j--;tmp--;
+			//std::cout << i << j << tmp << std::endl;
+			glp_load_matrix(lp,tmp,ia,ja,ar);
+			glp_simplex(lp,NULL);
 			bool wErr = false;
-			float *f = new float[N];
-			for(int i = 0;i < N; i++){
-				f[i] = 0;
-			}
-			for(int i = 0;i < N;i ++){
-				if(f[i] > ESP){
+			for(i = 0;i < N;i ++){
+				x[i+1] = glp_get_col_prim(lp,i+1);
+//				std::cout << x[i+1] << " ";
+				if(x[i+1] != 0){
 					wErr = true;
 					BEC += 1;
 				}
 			}
-			delete[] f;
+			glp_delete_prob(lp);
 			if(wErr)
 				WEC += 1;
-			if(WEC >= 50){
-				//fprintf
+			if(WEC >= 1){
+				printf("snr = %f,errCodeRate = %f,errFrameRate = %f\n",snr,(float)BEC/(N*sumCount),(float)WEC/sumCount);
+				fprintf(fp,"snr = %f,errCodeRate = %f,errFrameRate = %f\n",snr,(float)BEC/(N*sumCount),(float)WEC/sumCount);
 				break;
 			}
-break;
 		}
+		delete[] x;	
+		delete[] ia;
+		delete[] ja;
+		delete[] ar;
 	//	double frameErrRate = (double)errRate/sumCount;
 	//	fprintf(fp,"when snr = %lf,frame error rate = %lf\n",snr,frameErrRate);
 	//	printf("when snr=%lf,frame erroe rate=%lf\n",snr,frameErrRate);
 	}
-
+	fclose(fp);
 	return 0;
 }
